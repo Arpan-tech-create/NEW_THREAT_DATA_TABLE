6d9311f6cd9de6c2058755b16b4460ca773ed908
Arpan-tech-create <bhoiarpan123@gmail.com>
2023-06-22 20:39:35 +0530

    'bigchanges'

--- main.py
+++ main.py
@@ -1,59 +1,32 @@
 from flask import Flask, render_template, request
-import pandas as pd
-from datetime import datetime
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/')
-def show_table():
-    # Read the CSV file into a Pandas DataFrame
-    df = pd.read_csv('data.csv')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    conn = sqlite3.connect('vedas.db')
+    cur = conn.cursor()
+    
+    if request.method == 'POST':
+        selected_responses = request.form.getlist('response')
 
-    # Get the from_date and to_date from the request parameters
-    from_date = request.args.get('from_date')
-    to_date = request.args.get('to_date')
+        if '20x' in selected_responses:
+            selected_responses.extend(['200', '203'])
+        
+        if '40x' in selected_responses:
+            selected_responses.extend(['400', '403'])
 
-    # Get the checkbox values from the request parameters
-    response_40x = request.args.get('40x')
-    response_20x = request.args.get('20x')
-    response_50x = request.args.get('50x')
+        cur.execute('SELECT * FROM threat WHERE RESPONSE IN ({})'.format(','.join('?' * len(selected_responses))), selected_responses)
+        data = cur.fetchall()
 
-    # Create an empty DataFrame to store the filtered results
-    filtered_df = pd.DataFrame()
+    else:
+        cur.execute('SELECT * FROM threat')
+        data = cur.fetchall()
 
-    if response_40x:
-        filtered_df = pd.concat([filtered_df, df[df['RESPONSE'].astype(str).str.startswith('40')]])
-    if response_20x:
-        filtered_df = pd.concat([filtered_df, df[df['RESPONSE'].astype(str).str.startswith('20')]])
-    if response_50x:
-        filtered_df = pd.concat([filtered_df, df[df['RESPONSE'].astype(str).str.startswith('50')]])
+    conn.close()
 
-    # Filter the DataFrame based on the date range
-    if from_date and to_date:
-        df['TIMESTAMP'] = pd.to_datetime(df['TIMESTAMP'], format='%d/%b/%Y:%H:%M:%S:%z')
-        from_date = datetime.strptime(from_date, '%Y-%m-%d').date()
-        to_date = datetime.strptime(to_date, '%Y-%m-%d').date()
-        mask = (df['TIMESTAMP'].dt.date >= from_date) & (df['TIMESTAMP'].dt.date <= to_date)
-        filtered_df = filtered_df.loc[mask]
-
-    # Use the original DataFrame if no filters are applied
-    if not any([response_40x, response_20x, response_50x, from_date, to_date]):
-        filtered_df = df
-
-    # Convert the filtered DataFrame to an HTML table
-    table = filtered_df.to_html(classes='data', index=False)
-
-    # Construct the form action URL with selected checkboxes
-    url = request.base_url
-    if response_40x:
-        url += '&40x=' + response_40x
-    if response_20x:
-        url += '&20x=' + response_20x
-    if response_50x:
-        url += '&50x=' + response_50x
-
-    # Render the template with the table data
-    return render_template('table.html', table=table, form_action=url)
+    return render_template('table.html', data=data)
 
 
 if __name__ == '__main__':
